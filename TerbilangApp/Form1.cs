b79ce2596bbfc497fd68ac60d39ae97df8606328
Nurul Afini <43399744+nurulafini@users.noreply.github.com>
2018-10-12 19:39:22 +0700

    Add files via upload

---
+++ TerbilangApp/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TerbilangLibrary;
+
+namespace TerbilangApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int nominal = int.Parse(txtNominal.Text);
+            Terbilang terbilang = new Terbilang();
+
+            this.lstTerbilang.Items.Add(terbilang.TerbilangIndonesia(nominal));
+        }
+
+    }
+}
